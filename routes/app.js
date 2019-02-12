aa253c7629259bbe316926249ba390b430e42372
Gabriel Bitti <gabriel.bitti@live.com>
2019-02-12 00:31:46 +0000

    Adiciona nova view, cria novas rotas GET e POST

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.get('/message/:msg', function (req, res, next) {
+    res.render('node', {message: req.params.msg});
+});
+
+router.post('/message', function (req, res, next) {
+    var msg = req.body.message;
+    res.redirect('/message/' + msg);
+});
+
 module.exports = router;
