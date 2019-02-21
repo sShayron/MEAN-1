5d4b7e99b20ed3ab8957a04518621603d5414d2a
Shayron Aguiar <shayron.aguiar@gmail.com>
2019-02-21 20:02:14 -0300

    Update app.js

--- routes/app.js
+++ routes/app.js
@@ -3,17 +3,27 @@ var router = express.Router();
 var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.redirect('/create-user');
+});
+
+router.get('/get-user', function (req, res, next) {
+    res.render('get-user');
+});
+
+router.post('/api/get-user', function (req, res, next) {
+    const { email } = req.body;
+    const user = User.find({ email });
+    res.render('get-user', { user });
 });
 
 router.get('/create-user', function (req, res, next) {
     res.render('create-user');
 });
 
-router.post('/create-user', function (req, res, next) {
-    const { emafirstName, lastName, password, email } = req.body;
+router.post('/api/create-user', function (req, res, next) {
+    const { firstName, lastName, password, email } = req.body;
     const userObject = new User({
-        firstNam,
+        firstName,
         lastName,
         password,
         email
