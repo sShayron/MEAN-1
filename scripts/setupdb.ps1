b549326d0dcab041336e93a450c8732b2888a734
Shayron Aguiar <shayron.aguiar@gmail.com>
2019-02-25 22:03:09 -0300

    Update setupdb.ps1

--- scripts/setupdb.ps1
+++ scripts/setupdb.ps1
@@ -1,1 +1,1 @@
-'C:\Program Files\MEAN\MongoDB\Server\3.6\bin\mongod.exe' --dbpath C:\DevC\MEAN-1-master\data\db
+C:\"Program Files"\MEAN\MongoDB\Server\3.6\bin\mongod.exe --dbpath C:\DevC\MEAN-1-master\data\db
