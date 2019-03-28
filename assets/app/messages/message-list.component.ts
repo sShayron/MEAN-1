cbf3b55702b4fb86936574b91219a1716ba7ea05
Shayron Aguiar <shayron.aguiar@gmail.com>
2019-03-28 20:22:41 -0300

    Update message-list.component.ts

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MessageService } from './message.services';
 import { Message } from './message.model';
 
@@ -7,37 +7,18 @@ import { Message } from './message.model';
     templateUrl: './message-list.component.html',
     providers: [MessageService]
 })
-export class MessageListComponent {
+export class MessageListComponent implements OnInit {
     constructor(private messageService: MessageService) {
-        this.messageService.buckAddMessages(
-            [
-                new Message({
-                    messageId: 1,
-                    content: 'Message 1',
-                    author: 'author 1'
-                }),
-                new Message({
-                    messageId: 2,
-                    content: 'Message 2',
-                    author: 'author 2'
-                }),
-                new Message({
-                    messageId: 3,
-                    content: 'Message 3',
-                    author: 'author 3'
-                }),
-                new Message({
-                    messageId: 4,
-                    content: 'Message 4',
-                    author: 'author 4'
-                })
-            ]
-        )
     }
 
     copiedMessage: Message = new Message();
     isEditing: boolean = false;
     isAdding: boolean = false;
+
+    ngOnInit() {
+        // console.log('SEAJIOIJPESAPJIOSEAJOP');  
+        // this.messageService.getAllMessages();
+    }
 
     close() {
         this.isAdding = false;
