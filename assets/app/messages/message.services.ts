c739e1d98b9b0bd21cfc249f48e3d5a27feb76ef
Shayron Aguiar <shayron.aguiar@gmail.com>
2019-03-28 20:22:04 -0300

    Update message.services.ts

--- assets/app/messages/message.services.ts
+++ assets/app/messages/message.services.ts
@@ -1,19 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Message } from './message.model';
+import { Observable } from 'rxjs/Observable';
 
+@Injectable()
 export class MessageService {
     private messagesService: Message[] = [];
+
+    constructor(private http: HttpClient) { }
 
     getMessages() {
         return this.messagesService;
     }
 
-    addMessage(message: Message) {
+    getAllMessages() {
+        return this.http.get('/message/')
+            .subscribe((res: { data: Message[] }) => this.buckAddMessages(res.data));
+    }
+
+    addMessage(message: Message): Observable<any> {
         const idx = this.messagesService.map(m => m.messageId).indexOf(message.messageId);
         if (idx > -1) {
             console.error('Message already exists');
             return;
         }
-        this.messagesService.push(message);
+        this.http.post('/message/', { content: message.content })
+            .subscribe((res: { data: Message }) => this.messagesService.push(res.data));
     }
 
     removeMessage(message: Message) {
