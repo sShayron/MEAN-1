5562f7cd287eb1fa1f85fea1e09bb0e05cd70e00
shayron <shayron.aguiar@gmail.com>
2019-04-01 18:20:19 -0300

    fix: Remove duplicated folder

--- assets/app/assets/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-// https://angular.io/guide/ajs-quick-reference
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html'
-})
-export class AppComponent {
-}
